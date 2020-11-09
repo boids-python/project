9dfe118f0e744a3d1c70a63d574ca86ce1423184
Antonin Villepontoux <antoninvillepontoux34@gmail.com>
2020-11-09 15:25:22 +0100

    init instance move

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#coding:utf-8
+# coding:utf-8
 class Boids():
-	def __init__(self):
-		pass
+    # classe Boids initialisé
+    nb_boids = 0  # compteur nombre boids créer
+
+    def __init__(self):
+        # instance création boids
+        Boids.nb_boids += 1  # incrémente le compteur de boids de 1 à chaque création d'1 objet
+
+    def move(self, x, y, teta):
+        # instance déplacement en x,y et teta angle d'orientation
+        pass
